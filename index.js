a37ac240a9347b94026478f1241a3614cf975aee
Pierre Guillen <hi@cerbero.dev>
2020-07-19 00:56:44 -0500

    update

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
-const {YOUTUBE_API_KEY} = process.env
+const {YOUTUBE_API_KEY, INSTAGRAM_USER = 'midu.dev', INSTAGRAM_TOKEN = '.W1ldky0;***'} = process.env
 const fs = require('fs').promises
 const fetch = require('node-fetch')
 
 const Parser = require('rss-parser')
 const parser = new Parser()
 
+const Instagram = require('instagram-web-api')
+ 
+const client = new Instagram({username: INSTAGRAM_USER, password: INSTAGRAM_TOKEN})
 
+const NUM_OF_ARTICLES_TO_SHOW = 5
+const NUM_OF_PHOTOS_TO_SHOW = 4
 const NUM_OF_VIDEOS_TO_SHOW = 3
 
+const LATEST_ARTICLE_PLACEHOLDER = "%{{latest_articles}}%"
 const LATEST_YOUTUBE_VIDEOS = "%{{latest_youtube}}%"
+const LATEST_INSTAGRAM_PHOTO = "%{{latest_instagram}}%"
 // const LATEST_TWEET_PLACEHOLDER = "%{{latest_tweet}}%"
-// const LATEST_INSTAGRAM_PHOTO = "%{{latest_instagram}}%"
+
+const getPhotosFromInstagram = async () => {
+  await client.login()
+  const { user } = await client.getPhotosByUsername({username: 'midu.dev', first: NUM_OF_PHOTOS_TO_SHOW})
+  const {edge_owner_to_timeline_media: {page_info, edges}} = user
+  return edges
+}
 
 const getLatestYoutubeVideos = () => {
   return fetch(`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PL4DDrIrz67NApBMkJ4WLqxslYk-D4uJwt&maxResults=${NUM_OF_VIDEOS_TO_SHOW}&key=${YOUTUBE_API_KEY}`)
@@ -18,18 +31,23 @@ const getLatestYoutubeVideos = () => {
     .then(videos => videos.items)
 }
 
+const generateInstagramHTML = ({shortcode, thumbnail_src}) => `
+<a href='https://www.instagram.com/p/${shortcode}/' target='_blank'>
+  <img width='20%' src='${thumbnail_src}' alt='Instagram photo' />
+</a>`
+
 const generateYoutubeHTML = ({title, videoId}) => `
 <a href='https://youtu.be/${videoId}' target='_blank'>
   <img width='30%' src='https://img.youtube.com/vi/${videoId}/mqdefault.jpg' alt='${title}' />
 </a>`
 
 
-;
-(async () => {
-  const [template, videos] = await Promise.all([
+;(async () => {
+  const [template, {items: articles}, videos, photos] = await Promise.all([
     fs.readFile('./README.md.tpl', { encoding: 'utf-8' }),
-    parser.parseURL('#'),
-    getLatestYoutubeVideos()
+    //parser.parseURL('#'),
+    getLatestYoutubeVideos(),
+    getPhotosFromInstagram()
   ])
 
   // create latest article markdown
@@ -46,9 +64,16 @@ const generateYoutubeHTML = ({title, videoId}) => `
     })
     .join('')
 
+  // create latest photos from instagram
+  const latestInstagramPhotos = photos
+    .map(({node}) => generateInstagramHTML(node))
+    .join('')
+
   // replace all placeholders with info
   const newMarkdown = template
+    .replace(LATEST_ARTICLE_PLACEHOLDER, latestArticlesMarkdown)
     .replace(LATEST_YOUTUBE_VIDEOS, latestYoutubeVideos)
+    .replace(LATEST_INSTAGRAM_PHOTO, latestInstagramPhotos)
 
   await fs.writeFile('./README.md', newMarkdown)
 })()
